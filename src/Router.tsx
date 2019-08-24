7ad7eed480b07162e59bb6264f7af0032b425dc9
zhangxin <heermosi39@163.com>
2019-08-24 12:51:56 +0800

    feat(route): 增加组件lazy

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,38 +1,59 @@
-import React from 'react'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Redirect, Switch, Route } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { Redirect, Switch, Route, RouteProps } from 'react-router-dom'
 import { connect } from 'react-redux'
 import User from './pages/user'
 import Home from './pages/home/Home'
 import Detail from './pages/detail/Detail'
-import Nomatch from './pages/Nomatch'
+import Nomatch from './baseUI/Nomatch'
 import Content from './pages/home/Content'
-import * as UIComponents from './pages/ui'
+import allComponents from './pages/ui/index.md'
 
-interface UiType<T> {
-    [key: string]: T
-}
-const ui: UiType<any> = UIComponents
+const ui: React.ReactText[] = allComponents.trim().split(/[\t\n]+/)
 
 interface RouterProps {
     categoryList: any
 }
+interface ConfigItem {
+    key: string
+    name: React.ReactText
+}
 class AppRouter extends React.Component<RouterProps> {
-    render() {
+    routeComponents() {
         const categoryList = this.props.categoryList || []
         const routes = categoryList.reduce(
             (total: any[], v: { children: any }) =>
                 total.concat(v.children || []),
             []
         )
+        const result = routes.map((v: ConfigItem) => {
+            if (!ui.includes(v.name)) return null
+            const Component = lazy(() =>
+                import(
+                    /* webpackChunkName: "[request]" */
+                    `./pages/ui/${v.name}/index`
+                )
+            )
+            const ComponentWrap = (props: RouteProps) => {
+                return (
+                    <Suspense fallback={null}>
+                        <Component {...props} />
+                    </Suspense>
+                )
+            }
+            return (
+                <Route
+                    path={`/home${v.key}`}
+                    component={ComponentWrap}
+                    key={v.key}
+                />
+            )
+        })
+        return result
+    }
+    render() {
         return (
             <div>
-                <Route
-                    path="/"
-                    exact
-                    render={() => <Redirect to="/home" />}
-                    key="/"
-                />
+                <Route path="/" exact render={() => <Redirect to="/home" />} />
                 <Route path="/login" component={User} />
                 <Route
                     path="/home"
@@ -45,19 +66,7 @@ class AppRouter extends React.Component<RouterProps> {
                                     component={Content}
                                     exact
                                 />
-                                {routes.map(
-                                    (v: {
-                                        key: string
-                                        name: React.ReactText
-                                    }) =>
-                                        ({}.hasOwnProperty.call(ui, v.name) ? (
-                                            <Route
-                                                path={`/home${v.key}`}
-                                                component={ui[v.name]}
-                                                key={v.key}
-                                            />
-                                        ) : null)
-                                )}
+                                {this.routeComponents()}
                                 <Route component={Nomatch} />
                             </Switch>
                         </Home>
